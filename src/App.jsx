785e499ccc61e75eca487b896758ca4da1bdf88a
Mathias Appelmans <math.appelmans@gmail.com>
2025-11-17 11:37:43 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./routes/layout/Laayout";
+import Layout from "./routes/layout/Layout";
 import HomePage from "./routes/homePage/homePage";
 import ListPage from "./routes/listPage/listPage";
 import SinglePage from "./routes/singlePage/singlePage";
