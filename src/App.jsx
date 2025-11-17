f9546d58401b5c8724501293f09340f312e338f9
Mathias Appelmans <math.appelmans@gmail.com>
2025-11-17 11:35:33 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./routes/layout/Layout";
+import Layout from "./routes/layout/Laayout";
 import HomePage from "./routes/homePage/homePage";
 import ListPage from "./routes/listPage/listPage";
 import SinglePage from "./routes/singlePage/singlePage";
@@ -8,7 +8,7 @@ import LoginPage from "./routes/loginPage/loginPage";
 function App() {
 
     const router = createBrowserRouter([
-        {
+        {   
           path: "/",
           element: <Layout />,
           children: [
